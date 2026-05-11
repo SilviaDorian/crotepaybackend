5816399d8d7d90d4283c46e8b0b8496c8879be2c
DrWho234 <trustbridge74@gmail.com>
2026-05-11 12:39:59 +0100

    Fix: Cleaned up merge markers in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 import app from '../src/index.js';
 export default app;
-=======
-import app from '../src/index.js';
-export default app;
->>>>>>> 17f7b1a256438bb2fd1eda1e92380317ab0beccb
