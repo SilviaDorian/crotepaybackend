c9f59ecf7dfa675cc49e107d6efa939b6b512cee
DrWho234 <trustbridge74@gmail.com>
2026-05-30 01:53:22 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { query } from './db/index.js';
-import { createBulkEscrow } from './src/controllers/bulkControllers.js';
+import { createBulkEscrow } from './controllers/bulkControllers.js';
 
 
 // Import Routes
