4a33334d1db954d4acce7f3b1ec6c6193f59e281
DrWho234 <trustbridge74@gmail.com>
2026-07-04 14:18:42 +0100

    Describe your changes

--- src/routes/withdraw.js
+++ src/routes/withdraw.js
@@ -206,7 +206,7 @@ router.post('/verify-account', async (req, res) => {
 router.post('/withdraw/banks/NG', async (req, res) => {
     try {
         const response = await fetch('https://api.flutterwave.com/v3/banks/NG', {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Authorization': `Bearer ${process.env.FLW_SECRET_KEY}`
             }
